fb6743cb8e9543e1811bbff897b2671656d42bd2
Chris <moorecash@gmail.com>
2022-09-28 16:14:38 -0400

    added single source of truth

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import millify from 'millify'
-import { Typography } from 'antd'
+import { Typography, Row, Col, Statistic } from 'antd'
+import { Link } from 'react-router-dom'
 
+const { Title } = Typography
 const Homepage = () => {
   return (
-    <Typography.Title level={2} className='heading'>
-     
-    </Typography.Title>
+    <> 
+      <Title level={2} className='heading'>
+        Global Crypto Stats
+      </Title>
+      <Row>
+        <Col span={12}><Statistic title='Total CryptoCurrencies' value="5"/></Col>
+        <Col span={12}><Statistic title='Total Exchanges' value="5"/></Col>
+        <Col span={12}><Statistic title='Total Market Cap' value="5"/></Col>
+        <Col span={12}><Statistic title='Total 24h Volume' value="5"/></Col>
+        <Col span={12}><Statistic title='Total Markets' value="5"/></Col>
+
+      </Row>
+    </>
   )
 }
 
